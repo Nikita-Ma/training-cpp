4b10fa0b0eb7824024ac026534b773bbe2bee87b
Nik <nikitama1hr@gmail.com>
2023-06-26 14:25:29 +0300

    basic class & methods

--- variables.cpp
+++ variables.cpp
@@ -2,26 +2,39 @@
 // Created by Nik on 23.06.2023.
 //
 #include <iostream>
-void secondFunc (int qwe, int oqooq) {
+
+void secondFunc(int qwe, int oqooq) {
     qwe = 1;
     std::cout << qwe << std::endl;
 }
 
-void firstFunc (int a) {
+void firstFunc(int a) {
     int exampleF = 1;
     secondFunc(2, exampleF);
 }
 
+class TestClass {
+    int exampleVar;
+public:
+    void funcT();
+};
+
+void TestClass::funcT() {
+    std::cout << "test" << std::endl;
+}
+
 int main() {
 
-    std::string q = "qweqwe";
-    int globalVar = 1;
-
-    for (int i = 0; i < 10; ++i) {
-        int localVar = 2;
-        std::cout << q << std::endl;
-    }
-    firstFunc(2);
+//    std::string q = "qweqwe";
+//    int globalVar = 1;
+//
+//    for (int i = 0; i < 10; ++i) {
+//        int localVar = 2;
+//        std::cout << q << std::endl;
+//    }
+//    firstFunc(2);
+    TestClass testClass;
+    testClass.funcT();
 }
 
 
