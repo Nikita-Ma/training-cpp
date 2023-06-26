942923917b994e5ffab321c49ca49ed8935a483c
Nik <nikitama1hr@gmail.com>
2023-06-26 11:56:52 +0300

    basic func and stack

--- variables.cpp
+++ variables.cpp
@@ -2,12 +2,26 @@
 // Created by Nik on 23.06.2023.
 //
 #include <iostream>
+void secondFunc (int qwe, int oqooq) {
+    qwe = 1;
+    std::cout << qwe << std::endl;
+}
+
+void firstFunc (int a) {
+    int exampleF = 1;
+    secondFunc(2, exampleF);
+}
 
 int main() {
+
     std::string q = "qweqwe";
+    int globalVar = 1;
+
     for (int i = 0; i < 10; ++i) {
+        int localVar = 2;
         std::cout << q << std::endl;
     }
-
+    firstFunc(2);
 }
+
 
